8ccdad0a17ed0f917d78affb66dec58171abe034
augustin <augustin7766@gmail.com>
2024-07-21 00:33:02 +0530

    dataValidation

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import sys
 from src.semiconductor import logger, CustomException
 from src.semiconductor.pipeline.stage_01_dataIngestion import DataIngestionTrainingPipeline
-
+from src.semiconductor.pipeline.stage_02_dataValidation import DataValidationTrainingPipeline
 
 
 
@@ -14,5 +14,17 @@ try:
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
     logger.info(f'----------------------stage {STAGE_NAME} completed ---------------------')
+except Exception as e:
+    raise CustomException(e, sys)
+
+
+
+STAGE_NAME = 'Data Validation Stage'
+
+try:
+    logger.info(f'-----------------stage {STAGE_NAME} started-----------------------')
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f'-----------------stage {STAGE_NAME} completed-----------------------')
 except Exception as e:
     raise CustomException(e, sys)
--- src/semiconductor/config/configuration.py
+++ src/semiconductor/config/configuration.py
@@ -3,7 +3,7 @@
 
 from src.semiconductor.constants import *
 from src.semiconductor.utils.common import read_yaml, create_directories
-from src.semiconductor.entity.config_entity import (DataIngestionConfig)
+from src.semiconductor.entity.config_entity import (DataIngestionConfig, DataValidationConfig)
 from src.semiconductor import logger, CustomException
 
 
@@ -41,3 +41,21 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir = config.root_dir,
+            local_data_file= config.local_data_file,
+            STATUS_FILE = config.STATUS_FILE,
+            all_schema = schema
+        )
+
+        return data_validation_config
